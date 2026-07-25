d743bc13add3f148f34a513b7a01a819743d1cf0
appleweiping <vipinapple986@gmail.com>
2026-07-25 15:00:58 +0800

    chore: replace legacy site with al-folio upstream 309112cd

--- src/components/cabinet/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as CabinetApp } from "./CabinetApp";
-export { default } from "./CabinetApp";
-export type { CabinetAppProps, CabinetCapabilities, CabinetLocale, CabinetPhase } from "./types";
--- src/components/cabinet/wings.ts
+++
@@ -1,5 +0,0 @@
-import { getWing } from "../../data/wings";
-
-export function resolveCabinetWing(id: string) {
-  return getWing(id);
-}
